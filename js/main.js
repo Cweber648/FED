4a66861284348ba953b36acf9ffb8684ca76bffa
Charlie <Cweber648@gmail.com>
2018-02-01 11:09:24 -0600

    wanted to make this a git repository so i dont lose anything just in case

---
+++ js/main.js
@@ -0,0 +1,29 @@
+$(document).ready(function () {
+  //your code here
+  console.log("Hello World")
+  $('#image_2').hide();
+
+  setInterval(function() {
+    $("#image_1").fadeOut(1000)
+    $("#image_2").fadeIn(1000)
+    .end()
+    .prependTo('#image_1')
+  }, 3000);
+
+
+  $("#about_us_links").hide();
+  $("#about_us").on("click", function(){
+    $("#about_us_links").show();
+  })
+
+   $("#service_links").hide();
+  $("#service").on("click", function(){
+    $("#service_links").show();
+  })
+
+   $("#shop_links").hide();
+  $("#shop").on("click", function(){
+    $("#shop_links").show();
+  })
+})
+
---
+++ notes.md
@@ -0,0 +1,13 @@
+What is asked-
+ We need new fonts -----
+ we need to account for 5% of the browsers
+ we need to add icons to the set up
+ we need to set up the toggle menu in the bottom footer
+ Set up slide functionality
+
+1) included the fonts
+2) Added some min device specific pixel for retina displays
+3) Added a shopping cart icon and search bar button.
+4) all social media icons put in. Not sure if they want the actual text or just the icon.
+5) added mobile responsiveness
+6) Added some footer effects for the links.....
